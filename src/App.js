c5b78daf3982487b47dd442100caeec49ea37424
Anchal Mantwal <99482693+aanchalmantwal@users.noreply.github.com>
2024-05-28 07:01:04 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Empimage from './components/Empimage';
+import Emptable from './components/Emptable';
+
+function App() {
+  return (
+    <div className="App">
+  {/* Render Emptable and Empimage components */}
+   <Emptable />
+   <Empimage />
+    </div>
+  );
+}
+export default App;
